b2acbebd49f46b998e4bd2efa32458cc98248849
gera <zkingchannel@gmail.com>
2022-06-25 17:45:39 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript 1', body: 'prosto ohuet blyat' },
+    { id: 2, title: 'Javascript 2', body: 'prosto ohuet blyat' },
+    { id: 3, title: 'Javascript 3', body: 'prosto ohuet blyat' },
+  ])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type='text' placeholder='Post name' />
+        <input type='text' placeholder='Post description' />
+        <MyButton disabled>Create post</MyButton>
+      </form>
+      <PostList posts={posts} title="Posts list 3" />
     </div>
   );
 }
